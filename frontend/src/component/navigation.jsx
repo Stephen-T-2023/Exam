bc1cd6766b63027402bc85df725727d6d39dd10b
Stephen-T-2023 <144911216+Stephen-T-2023@users.noreply.github.com>
2025-01-20 15:40:41 +0000

    basic ticket system, no backend

--- frontend/src/component/navigation.jsx
+++ frontend/src/component/navigation.jsx
@@ -1,23 +1,16 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import React, { useState, useEffect} from 'react';
 
 const Navigation = () => {
-
-    const [isAuth, setIsAuth] = useState(false);
-    useEffect(() => {
-        if (localStorage.getItem('access_token') !== null) {
-        setIsAuth(true); 
-        }
-    }, [isAuth]);
 
     return ( 
         <div>
             <Navbar bg="dark" variant="dark" className='text-white flex w-screen bg-slate-400'>
-                <Navbar.Brand href="/" className='flex-grow'>Riget Zoo Adventures</Navbar.Brand>
-                <Nav>
-                    {isAuth ? <Nav.Link href="/logout" >Logout</Nav.Link> : <Nav.Link href="/login">Login</Nav.Link>}
-                </Nav>
+                <Navbar.Brand href="/" className='flex-grow border-black border-2'>Riget Zoo Adventures</Navbar.Brand>
+                <Nav.Link href='/bookingchoice' className='px-5 border-black border-2'>Shop</Nav.Link>
+                <Nav.Link href='/map' className='px-5 border-black border-2'>Map</Nav.Link>
+                <Nav.Link href='/about' className='px-5 border-black border-2'>About</Nav.Link>
+                <Nav.Link href="/logout" className='px-5 border-black border-2'>Logout</Nav.Link>
             </Navbar>
         </div>
         );
--- frontend/src/component/tickets.jsx
+++ frontend/src/component/tickets.jsx
@@ -11,28 +11,28 @@ const Tickets = () => {
 
 
     return ( 
-        <div>
+        <div className="flex flex-wrap flex-col w-screen h-screen bg-lime-300">
             <Navigation></Navigation>
-            <div>Tickets:</div>
-            <div>
-                <h2>Adult</h2>
-                <p>price and that <button onClick={() => {setAdult(adult+1), setPrice(price+10)}}>{adult}</button></p>
+            <div className="flex bg-red-600 w-full text-6xl text-left content-center font-heading">Tickets:</div>
+            <div className="w-full h-1/5 place-items-center content-center border-black border-2 text-2xl font-body">
+                <h2>Adult Tickets</h2>
+                <p>Age 16 and over: £20.00 <button className="border-2 border-black" onClick={() => {setAdult(adult+1), setPrice(price+10)}}>{adult}</button></p>
             </div>
-            <div>
-                <h2>Young Person</h2>
-                <p>price and that <button onClick={() => {setYoungPerson(youngperson+1), setPrice(price+8)}}>{youngperson}</button></p>
+            <div className="w-full h-1/5 place-items-center content-center border-black border-2 text-2xl font-body">
+                <h2>Young Person Tickets</h2>
+                <p>Between Ages 12 and 15: £15.00 <button className="border-2 border-black" onClick={() => {setYoungPerson(youngperson+1), setPrice(price+8)}}>{youngperson}</button></p>
             </div>
-            <div>
-                <h2>Child</h2>
-                <p>price and that <button onClick={() => {setChild(child+1), setPrice(price+5)}}>{child}</button></p>
+            <div className="w-full h-1/5 place-items-center content-center border-black border-2 text-2xl font-body">
+                <h2>Child Tickets</h2>
+                <p>Between Ages 3 and 12: £10.00 <button className="border-2 border-black" onClick={() => {setChild(child+1), setPrice(price+5)}}>{child}</button></p>
             </div>
-            <div>
-                <h2>Infant</h2>
-                <p>price and that <button onClick={() => {setInfant(infant+1)}}>{infant}</button></p>
+            <div className="w-full h-1/5 place-items-center content-center border-black border-2 text-2xl font-body">
+                <h2>Infant Tickets</h2>
+                <p>Age 2 and under: Free <button className="border-2 border-black" onClick={() => {setInfant(infant+1)}}>{infant}</button></p>
             </div>
-            <h2>Total Price: <h2>£{price}</h2></h2>
+            <h2 className="w-full h-auto place-items-center border-black border-2 text-2xl font-body">Total Price: <h2>£{price}</h2></h2>
         </div>
-     );
+    );
 }
- 
+
 export default Tickets;
